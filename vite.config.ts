ce9aa7ab895a35a3e4f398b13a71306464984c98
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-15 22:40:13 -0300

    chore: create aliases for components, common, theme and lib folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@common': path.resolve(__dirname, './src/common/'),
+      '@theme': path.resolve(__dirname, './src/theme'),
+      '@lib': path.resolve(__dirname, './src/lib/'),
+    },
+  },
   plugins: [react()],
-})
+});
