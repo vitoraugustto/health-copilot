3c4f7113ebf943b0bc41f5ac4c247cef103f92cc
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-17 16:59:00 -0300

    feat: create chatCompletion openai service

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
-import { OPENAI_API_KEY } from '@config/api';
+import { OPENAI_API_KEY, OPENAI_CONTEXT } from '@config/api';
 
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
@@ -14,3 +14,13 @@ export function transcribe(file: File) {
     prompt: 'Transcreva sempre para português.',
   });
 }
+
+export const chatCompletion = (prompt: string) => {
+  return openai.chat.completions.create({
+    messages: [
+      { role: 'system', content: OPENAI_CONTEXT },
+      { role: 'user', content: prompt },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+};
